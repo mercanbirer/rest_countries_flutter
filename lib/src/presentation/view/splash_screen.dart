ba55253eb4398d32d8586a0232b978d0a1660043
Mercan Nur Birer <mercanbirer42@gmail.com>
2023-10-03 16:45:10 +0300

    added splash screen

---
+++ lib/src/presentation/view/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return const HomePage();
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/country-flags.jpg'),
+      ),
+    );
+  }
+}
